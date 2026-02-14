c28804a0c0369b033afa29e2189e84a9bad36e87
linqu191-collab <linqu191@gmail.com>
2026-02-14 18:20:54 +0800

    添加后端入口

---
+++ server/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  
+  app.setGlobalPrefix('api')
+  app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
+  
+  const port = process.env.PORT || 3000
+  await app.listen(port)
+  
+  console.log(`Application is running on: http://localhost:${port}`)
+}
+
+bootstrap()
